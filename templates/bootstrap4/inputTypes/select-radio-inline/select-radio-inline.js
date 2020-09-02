ff6f3d90dc31b1172c4029c8b1c87c9d12d0c928
Jan Küster <jkuester@uni-bremen.de>
2020-09-02 15:04:36 +0200

    input type select-radio-inline uses bs4 custom-radio-inline skin

--- templates/bootstrap4/inputTypes/select-radio-inline/select-radio-inline.js
+++ templates/bootstrap4/inputTypes/select-radio-inline/select-radio-inline.js
@@ -3,13 +3,19 @@ import { omit } from '../../../../utils/omit'
 import './select-radio-inline.html'
 
 Template.afRadioGroupInline_bootstrap4.helpers({
-  atts () {
+  atts (index) {
     const atts = omit(this.atts, 'data-schema-key')
     if (this.selected) {
       atts.checked = ''
     }
+    
     atts.class = atts.class || ''
-    atts.class += ' form-check-input'
+    atts.class = `${atts.class} form-check-input custom-control-input`
+
+    if (atts.id) {
+      atts.id = `${atts.id}-${index}`
+    }
+
     return atts
   },
   dsk () {
