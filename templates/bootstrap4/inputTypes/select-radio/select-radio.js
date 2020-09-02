d06c6aa62a36e3992353b3aec19b933c24a0ac53
Jan Küster <jkuester@uni-bremen.de>
2020-09-02 14:58:21 +0200

    input type select-radio uses bs4 custom-radio skin

--- templates/bootstrap4/inputTypes/select-radio/select-radio.js
+++ templates/bootstrap4/inputTypes/select-radio/select-radio.js
@@ -4,6 +4,12 @@ import { dataSchemaKey } from '../../../../utils/dataSchemaKey'
 import './select-radio.html'
 
 Template.afRadioGroup_bootstrap4.helpers({
-  atts: selectedAttsAdjustGroup,
+  atts: function (index) {
+    const saag = selectedAttsAdjustGroup.call(this)
+    if (saag.id) {
+      saag.id = `${saag.id}-${index}`
+    }
+    return saag
+  },
   dsk: dataSchemaKey
 })
