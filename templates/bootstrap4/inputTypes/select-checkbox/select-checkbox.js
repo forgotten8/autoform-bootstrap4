d518a61bf6674d1f95aced5324a94e55c59945cd
Jan Küster <jkuester@uni-bremen.de>
2020-09-02 15:13:21 +0200

    input type select-checkbox uses bs4 custom-checkbox skin

--- templates/bootstrap4/inputTypes/select-checkbox/select-checkbox.js
+++ templates/bootstrap4/inputTypes/select-checkbox/select-checkbox.js
@@ -4,6 +4,13 @@ import { dataSchemaKey } from '../../../../utils/dataSchemaKey'
 import './select-checkbox.html'
 
 Template.afCheckboxGroup_bootstrap4.helpers({
-  atts: selectedAttsAdjustGroup,
+  atts: function (index) {
+    const saag = selectedAttsAdjustGroup.call(this)
+    if (saag.id) {
+      saag.id = `${saag.id}-${index}`
+    }
+    saag.class = `${saag.class || ''} custom-control-input`
+    return saag
+  },
   dsk: dataSchemaKey
 })
